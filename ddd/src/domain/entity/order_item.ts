2cc2826b6502479050a80107773c47b276db5045
Vitor Lourenço <vitor.brother17@outlook.com.br>
2022-10-08 14:40:31 -0300

    feat: challenge done

--- ddd/src/domain/entity/order_item.ts
+++ ddd/src/domain/entity/order_item.ts
@@ -1,35 +1,49 @@
-export default class OrderItem{
-  private _id: string
-  private _name: string
-  private _price: number
-  private _productId: string
-  private _quantity: number
+export default class OrderItem {
+  private _id: string;
+  private _productId: string;
+  private _name: string;
+  private _price: number;
+  private _quantity: number;
 
-  constructor(id: string, name: string, price: number, _productId: string, quantity: number){
-    this._id = id
-    this._name = name
-    this._price= price
-    this._productId = _productId
-    this._quantity = quantity
+  constructor(
+    id: string,
+    name: string,
+    price: number,
+    productId: string,
+    quantity: number
+  ) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this._productId = productId;
+    this._quantity = quantity;
   }
 
-  get id():string{
-    return this._id
+  get id(): string {
+    return this._id;
   }
 
-  get name():string{
-    return this._name
+  get name(): string {
+    return this._name;
   }
 
-  get productId():string{
-    return this._productId
+  get productId(): string {
+    return this._productId;
   }
 
-  get quantity():number {
-    return this._quantity
+  get quantity(): number {
+    return this._quantity;
   }
 
-  get price():number{
-    return this._price * this._quantity
+  get price(): number {
+    return this._price; // * this._quantity;
+  }
+
+  get subTotalPrice(): number {
+    return this._price * this._quantity;
+  }
+
+  totalPrice(): number {
+    return this._price * this._quantity;
   }
 }
--- ddd/src/infrastructure/repository/order.repository.ts
+++ ddd/src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
+import OrderRepositoryInterface from "../../domain/repository/order-respository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -19,5 +21,104 @@ export default class OrderRepository {
       },
       { include: [{ model: OrderItemModel }] }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    const order = await OrderModel.findOne({
+      where: { id: entity.id },
+      include: ["items"],
+      rejectOnEmpty: true,
+    });
+
+    const destroyOrderItemsPromise = order.items.map((item) =>
+      OrderItemModel.destroy({
+        where: {
+          id: item.id,
+        },
+      })
+    );
+    await Promise.all(destroyOrderItemsPromise);
+
+    const addOrderItemsPromise = entity.items.map((item) => {
+      const { id, name, price, productId, quantity } = item;
+      return OrderItemModel.create({
+        id,
+        name,
+        price,
+        product_id: productId,
+        quantity,
+        order_id: entity.id,
+      });
+    });
+    await Promise.all(addOrderItemsPromise);
+
+    await OrderModel.update(
+      {
+        customer_id: entity.customerId,
+        total: entity.total(),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+  }
+
+  async find(id: string): Promise<Order> {
+    let orderModel;
+
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: [{ model: OrderItemModel }],
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const order = new Order(
+      id,
+      orderModel.customer_id,
+      orderModel.items.map<OrderItem>(
+        (item) =>
+          new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          )
+      )
+    );
+
+    return order;
+  }
+
+  async findAll(): Promise<Order[]> {
+    const ordersDb = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    });
+
+    const orders = ordersDb.map((order) => {
+      return new Order(
+        order.id,
+        order.customer_id,
+        order.items.map((item) => {
+          return new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          );
+        })
+      );
+    });
+
+    return orders;
   }
 }
